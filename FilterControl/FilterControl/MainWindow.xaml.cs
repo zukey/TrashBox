4dae4302b96bee3d46992f9cf68d0feeb09a57e5
zukey <valenciada2000@gmail.com>
2014-01-22 22:18:32 +0900

    WPFのゴミ箱追加

---
+++ FilterControl/FilterControl/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilterControl
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var panel = this.FindName("pnlItems2") as StackPanel;
+
+            var chk = new CheckBox();
+            chk.Content = "Addtion";
+            chk.Height = 20;
+            chk.Width = 88;
+
+            var cmb = new ComboBox();
+            cmb.Height = 25;
+            cmb.Width = 123;
+
+            var newPanel = new StackPanel();
+            newPanel.Orientation = Orientation.Horizontal;
+            newPanel.Margin = new Thickness(5, 15, 5, 5);
+            newPanel.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            newPanel.Children.Add(chk);
+            newPanel.Children.Add(cmb);
+
+            panel.Children.Add(newPanel);
+
+        }
+    }
+}
---
+++ FilterControl/FilterControl/SampleData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterControl
+{
+    class SampleData
+    {
+        public string Name { get; set; }
+        public int Val { get; set; }
+        public SampleEnum Selection { get; set; }
+    }
+
+    enum SampleEnum
+    {
+        Item1,
+        Item2,
+        Item3,
+    }
+}
